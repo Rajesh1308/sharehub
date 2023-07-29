1d951e0f5f3814d493ff96a5ddc1877e4ca4d137
Rajesh1308 <rajesharulrathna@gmail.com>
2023-07-29 09:56:16 +0530

    Details added

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Share Hub")
+
+st.write("""Welcome to Share Hub! Share Hub is an application that allows you to share your 
+         files with your friends and family.""")
+
+st.subheader("Problem Statement")
+
+st.write("""In conventional way, data sharing is done by using pen drives, hard disks, etc. 
+         Within a network (of organization) data sharing is done through cables via shared 
+         files or mails. In some cases internet is used to share data. If the organization 
+         is practicing BYOD then data sharing is done through cloud for which internet connnection 
+         is mandatory. """)
+         
+st.subheader("Application Features")
+
+st.write("""Share hub application will be in running in all the clients within a network which 
+         can facilitae data sharing which uses the concept of shared resoures file sharingmethod through wireless medium.""")
+
